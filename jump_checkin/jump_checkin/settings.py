29856b46eee602b9aada875d2fad8fa87155de14
duduetc <eduardo.etca@gmail.com>
2025-06-10 16:39:27 -0300

    correção da localização da pasta templates

--- jump_checkin/jump_checkin/settings.py
+++ jump_checkin/jump_checkin/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(bz^%k=@g7)eu=gxwmnp63l)e8v_p6sxtwcm(czx07ef(8pta0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
