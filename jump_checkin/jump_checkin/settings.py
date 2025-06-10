86350ab27a6aaf6acb521cb803d357e33abb7ebf
duduetc <eduardo.etca@gmail.com>
2025-06-10 16:23:17 -0300

    correção do settings e urls

--- jump_checkin/jump_checkin/settings.py
+++ jump_checkin/jump_checkin/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'adolescentes',  # App de adolescentes
 ]
 
 MIDDLEWARE = [
--- jump_checkin/jump_checkin/urls.py
+++ jump_checkin/jump_checkin/urls.py
@@ -19,6 +19,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
