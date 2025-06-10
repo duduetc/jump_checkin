d6932cc1d197578b679230ed90bc20491a5a90c6
duduetc <eduardo.etca@gmail.com>
2025-06-10 16:32:23 -0300

    alteração para rodar local

--- jump_checkin/jump_checkin/settings.py
+++ jump_checkin/jump_checkin/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(bz^%k=@g7)eu=gxwmnp63l)e8v_p6sxtwcm(czx07ef(8pta0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
