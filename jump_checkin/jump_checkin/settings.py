89eb1292900060d97e0c0e02759a02d5a05efed1
duduetc <112420455+duduetc@users.noreply.github.com>
2025-06-18 19:11:22 -0300

    Update settings.py

--- jump_checkin/jump_checkin/settings.py
+++ jump_checkin/jump_checkin/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-(bz^%k=@g7)eu=gxwmnp6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jump-checkin.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
